d1f91d075b0e9d2f7ac3fcc9a4229a23e9c1d98a
datnguyen79198 <datnguyen79198@gmail.com>
2021-09-20 15:41:33 +0300

    added arguments parser

--- llvm_jit.py
+++ llvm_jit.py
@@ -1,5 +1,6 @@
 import sys 
 import os
+import argparse 
 
 import llvmlite.binding as llvm
 from llvmlite import ir
@@ -7,24 +8,25 @@ from llvmlite import ir
 from AST import ast_parsing
 from utils import file_op
 
-if __name__ == "__main__":
-    if len(sys.argv) < 1:
-        raise TypeError("Missing file !!") 
-    
-    FILENAME_IN = sys.argv[1] 
-
-
-    FILENAME_OUT = ""
-    if len(sys.argv) > 2:
-        FILENAME_OUT = sys.argv[2]
-
-    source = file_op.read_from_file(FILENAME_IN)
-    
+def build_AST_tree(IN, OUT):
+    source = file_op.read_from_file(IN)
     ast_tree = ast_parsing.dump(source)
 
-    if FILENAME_OUT == "":
+    if OUT == None:
         print(ast_tree)
     else:
-        file_op.write_to_file(FILENAME_OUT, ast_tree)
-    
+        file_op.write_to_file(OUT, ast_tree)
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser() 
+    arg_parser.add_argument("--AST",required=False) 
+    arg_parser.add_argument("--input", required=True)
+    arg_parser.add_argument("--output",required=False)
+    args = arg_parser.parse_args()
+
+    if args.AST == "True":
+        FILENAME_IN = args.input
+        FILENAME_OUT = args.output
+        build_AST_tree(FILENAME_IN, FILENAME_OUT)
+      
 
