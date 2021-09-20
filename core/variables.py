c2ed6eded962b6b3032ca057d1f99620af656149
datnguyen79198 <datnguyen79198@gmail.com>
2021-09-20 16:11:55 +0300

    variables enherited

--- core/variables.py
+++ core/variables.py
@@ -2,8 +2,28 @@
 import ast 
 
 class Var(ast.AST):
-    #ctx: Load/Store/Del
+    #ctx: Load/Store/Del/Starred
     def __init__(self, id, ctx=None):
         self._id = id 
         self._ctx = ctx
         self._fields = ["id", "ctx"]
+    
+    @property
+    def id(self):
+        return self._id 
+    
+    @id.setter
+    def id(self,n_id):
+        self._id = n_id 
+
+    @property
+    def ctx(self):
+        return self._ctx 
+    
+    @ctx.setter
+    def ctx(self,n_ctx):
+        self._ctx = n_ctx
+    
+    @property
+    def fields(self):
+        return self._fields
