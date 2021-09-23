b4491f76e135dfa25d9ee69aff5f0ba576f9ee2a
datnguyen79198 <datnguyen79198@gmail.com>
2021-09-23 16:49:29 +0300

    loop type inference

--- types/inference.py
+++ types/inference.py
@@ -46,14 +46,12 @@ class TypeInference(object):
             return self.visit_generic(node)
 
     def visit_Var(self, node, dtype = None):
-        if dtype is not None:
-            node.dtype = dtype 
+        if (dtype is not None) and (node.id not in self._cache):
             self._cache[node.id] = dtype
-        else:
-            if node.id in self._cache:
-                node.dtype = self._cache[node.id] 
-        print("* var *", vars(node))
-        return node
+        
+        node.dtype = self._cache[node.id] 
+        print("* var *", node , "\n" , vars(node))
+        return node.dtype
     
     def visit_Int(self, node, attrs = None):
         node.dtype = self.get_name() 
@@ -67,22 +65,42 @@ class TypeInference(object):
         #Int/Float --> $ptr
         dtype_value = self.visit(node.value)
         
-        targets = self.visit(node.targets) 
+        targets = node.targets
         if targets.id in self._cache:
             # y = x , x = $ptr => y = $ptr
             self._equal_relation.append((dtype_value, self._cache[targets.id])) 
-        
-        self._cache[targets.id] = dtype_value
 
         _ = self.visit(node.targets, dtype_value)
         return None
+
+    def visit_args(self, args):
+        type_args = [self.get_name() for _ in args] 
+
+        i = 0
+        for arg, type_arg in zip(args, type_args):
+            type_args[i] = self.visit(arg, type_arg)
+            i += 1
+        
+        return type_args
+
+    def visit_Call(self, node, attrs = None):
+        #node.func = name of func called 
+        #will be touch later 
+        type_args = self.visit_args(node.args) 
+        return type_args
+
+    def visit_For(self, node, attrs = None):
+        for_id = self.visit(node.target, int32)
+        range_bounded = self.visit(node.iter)
+        
+        self._equal_relation.append((for_id,int32)) 
+        self._equal_relation.extend([(b,int32) for b in range_bounded])
+        print(self._equal_relation)
+        print(self._cache)
     
     def visit_FunctionDef(self, node, attrs = None):
-        type_args = [self.get_name() for _ in node.args] 
+        type_args = self.visit_args(node.args)
         type_ret = TVar("$ret")
-
-        for (arg, type_arg) in zip(node.args, type_args):
-            _ = self.visit(arg, type_arg)
         
         _ = list(map(self.visit, node.body)) 
 
@@ -94,7 +112,8 @@ class TypeInference(object):
 if __name__ == "__main__":
     def addup(n,a,b):
         x = 1
-        for i in range(n):
+        a = 2
+        for i in range(a,n):
             n += 1 + x
         return n
 
