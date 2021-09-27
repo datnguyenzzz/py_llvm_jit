c4c16770ad81495a74c1c1ea608d5b3154cf5ea1
datnguyen79198 <datnguyen79198@gmail.com>
2021-09-27 15:39:00 +0300

    LLVM builder properties

--- IRBuilder/Emitter.py
+++ IRBuilder/Emitter.py
@@ -1,3 +1,58 @@
+#IRBuilder is the workhorse of LLVM Intermediate representation 
+#(IR) generation. It allows you to fill the basic blocks of your functions with LLVM instructions.
+
+from collections import defaultdict
+
 class LLVMEmitter(object):
-    def __init__(self):
-        pass
+    #Contains properties : 
+    #self._function : LLVM Function 
+    #self._builder: LLVM Builder 
+    #self._locals: Local variables
+    #self._meta_data: Array of metadatas 
+    #self._exit_block: Exit block 
+    #self._spec_types: type specialization 
+    #self._ret_type: return type 
+    #self._arg_types: Argument types 
+
+    def __init__(self), spec_types, ret_type, arg_types):
+        self._function = None 
+        self._builder = None 
+        self._locals = {} 
+        self._meta_data = defaultdict(dict)
+        self._exit_block = None 
+        self._spec_types = spec_types 
+        self._ret_type = ret_type 
+        self._arg_types = arg_types
+    
+    @property 
+    def function(self):
+        return self._function
+    
+    @property 
+    def builder(self):
+        return self._builder
+
+    @property 
+    def locals(self):
+        return self._locals
+    
+    @property 
+    def meta_data(self):
+        return self._meta_data
+
+    @property 
+    def exit_block(self):
+        return self._exit_block
+
+    @property 
+    def spec_types(self):
+        return self._spec_types
+    
+    @property 
+    def ret_type(self):
+        return self._ret_type
+    
+    @property 
+    def _arg_types(self):
+        return self.arg_types
+    
