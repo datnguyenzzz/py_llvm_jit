3c52b649e3555713a3017c8b7c29b6344482d77d
datnguyen79198 <datnguyen79198@gmail.com>
2021-09-27 16:55:30 +0300

    mapping basics type to llvm

--- LLVMIRBuilder/LLVM_types.py
+++ LLVMIRBuilder/LLVM_types.py
@@ -1,3 +1,8 @@
+import sys 
+sys.path.append("../")
+
+from custom_types.basics import *
+
 from llvmlite import ir
 
 pointer = ir.PointerType
@@ -9,3 +14,15 @@ bool_type = ir.IntType(1)
 
 void_type = ir.VoidType()
 void_ptr = pointer(ir.IntType(8))
+
+# LLVM types mapping
+
+llvm_types_map = {
+    int32 : int_type, 
+    int64 : int_type,
+    float32 : float_type,
+    double64 : double_type,
+} 
+
+def to_llvm_type(dtype):
+    return llvm_types_map[dtype]
