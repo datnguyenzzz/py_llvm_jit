5de0880d8890843e7334fd33d0e04727a32974c9
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-16 16:33:34 +0300

    procedure only function didn't in cache

--- compiler/codegen.py
+++ compiler/codegen.py
@@ -8,6 +8,8 @@ import numpy as np
 from optimizes import unification
 from custom_types.basics import *
 
+FUNC_CACHE = {}
+
 def arg_py_type(arg):
     if isinstance(arg, int) and arg <= sys.maxsize:
         return int64 
@@ -53,7 +55,12 @@ def recompile(args, ast, infer_type, mgu):
     spec_args = [unification.apply(specialization, arg) for arg in type_args]
 
     if determined(spec_ret) and all(map(determined, spec_args)):
-        print("Yay")
+        func_name = "".join([ast.name,str(hash(tuple(spec_args)))])
+
+        if func_name in FUNC_CACHE:
+            return FUNC_CACHE[func_name](*args)
+        else:
+            pass
     else:
         raise Exception('Some argument has not been determined')
 
